58cffb68d0c7cf2086cb25df5bde3afb56e8726d
jaylomp <jaylom.pairol@ufl.edu>
2023-03-27 23:27:46 -0400

    second commit

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-print()
+
+
+
+def encode(password):
+    encoded_password = ''
+    for digit in password:
+        encoded_digit = (int(digit) + 3) % 10
+        encoded_password += str(encoded_digit)
+    return encoded_password
+
+
+def decode(password):
+    pass
+
+
+if __name__ == '__main__':
+    while True:
+        print()
+        print('Menu \n------------- \n1. Encode \n2. Decode \n3.Quit')
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            password = input('Please enter your password to encode: ')
+            encode(password)
+            print('Your password has been encoded and stored!')
+        elif option == 2:
+            pass
+            print(f'The encoded password is {encode(password)}, and the original password is')
+        elif option == 3:
+            break
+
